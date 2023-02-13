995662ac1a79e521f5c00d787297215267f23db2
João Matos <joaocarlosm00@gmail.com>
2023-02-13 00:26:40 -0500

    Add Lactate values, Hemoglobin, and Blood Transfusion

--- src/sql/eICU/eICU_lactate.sql
+++ src/sql/eICU/eICU_lactate.sql
@@ -1,6 +1,135 @@
+WITH lac AS (
+
+  SELECT 
+    patientunitstayid
+  , chartoffset
+  , lactate
+  , ROW_NUMBER() OVER(PARTITION BY patientunitstayid ORDER BY chartoffset ASC) AS total_seq
+  , CASE
+      WHEN chartoffset >= -1440 AND chartoffset < 240    THEN 0 -- we'll take day 0 up to first 4 hours
+      WHEN chartoffset >= 240   AND chartoffset < 1440   THEN 1
+      WHEN chartoffset >= 1440  AND chartoffset < 1440*2 THEN 2
+      ELSE NULL
+    END AS day
+
+  FROM `physionet-data.eicu_crd_derived.pivoted_lab`
+  WHERE lactate IS NOT NULL
+
+),
+
+-- add sequence within the day, but inverted
+lacs AS (
+
+  SELECT *
+
+  , ROW_NUMBER() OVER(PARTITION BY patientunitstayid, day ORDER BY chartoffset DESC) AS day_prox
+
+  FROM lac
+
+),
+
+lac_0 AS (
+
+  SELECT 
+
+    patientunitstayid
+  , lactate AS lactate_day0
+
+  FROM lacs
+  WHERE day = 0
+  AND day_prox = 1 -- aka the closest value to our upper time threshold
+),
+
+lac_1 AS (
+
+  SELECT 
+
+    patientunitstayid
+  , MAX(lactate) AS lactate_day1
+  , COUNT(lactate) AS lactate_freq_day1
+
+  FROM lacs
+  WHERE day = 1
+  GROUP BY patientunitstayid
+),
+
+lac_2 AS (
+
+  SELECT 
+
+    patientunitstayid
+  , MAX(lactate) AS lactate_day2
+  , COUNT(lactate) AS lactate_freq_day2
+
+  FROM lacs
+  WHERE day = 2
+  GROUP BY patientunitstayid
+),
+
+hb AS (
+
+  SELECT
+    patientunitstayid
+  , MIN(hemoglobin) AS hemoglobin_stay_min
+
+  FROM `physionet-data.eicu_crd_derived.pivoted_lab`
+  WHERE hemoglobin IS NOT NULL
+  AND chartoffset > -1440 -- at least 1 day before the stay
+  GROUP BY patientunitstayid
+  ORDER BY patientunitstayid
+
+),
+
+transf AS (
+  SELECT
+      patientunitstayid
+    , intakeoutputoffset
+    , CASE
+        WHEN intakeoutputoffset >= 0     AND intakeoutputoffset < 1440   THEN 1
+        WHEN intakeoutputoffset >= 1440  AND intakeoutputoffset < 1440*2 THEN 2
+      ELSE NULL
+    END AS day
+    , cellvaluenumeric
+
+  FROM `physionet-data.eicu_crd.intakeoutput`
+
+  WHERE celllabel = "Volume (ml)-Transfuse - Leukoreduced Packed RBCs"
+     OR celllabel = "Volume-Transfuse red blood cells"
+     OR LOWER(celllabel) LIKE "%rbc%"
+),
+
+transfusion_1 AS (
+
+  SELECT
+
+    patientunitstayid
+  , 1 AS transfusion_yes
+  , SUM(cellvaluenumeric) AS transfusion_units_day1
+
+  FROM transf
+  WHERE day = 1
+
+  GROUP BY patientunitstayid
+),
+
+transfusion_2 AS (
+
+  SELECT
+
+    patientunitstayid
+  , 1 AS transfusion_yes
+  , SUM(cellvaluenumeric) AS transfusion_units_day2
+
+  FROM transf
+  WHERE day = 2
+
+  GROUP BY patientunitstayid
+)
+
+
 SELECT DISTINCT
 
-   1 AS sepsis3 -- all patients from Yugang's table are already septic
+    1 AS sepsis3 -- all patients from Yugang's table are already septic
   , yug.hospitalid AS hospital_id
   , yug.wardid AS icu_id
   , yug.patienthealthsystemstayid AS patient_id
@@ -41,7 +170,14 @@ SELECT DISTINCT
 
   , COALESCE(icustay_detail.admissionweight, icustay_detail.dischargeweight) AS weight
   
-  , yug.hospitaldischargeoffset / 1440 AS los_icu_hours
+  , lactate_day0
+  , lactate_day1
+  , lactate_freq_day1
+  , lactate_day2
+  , lactate_freq_day2
+  
+  , hemoglobin_stay_min 
+
   , CASE 
       WHEN yug.unitdischargelocation = "Death"
         OR yug.unitdischargestatus = "Expired"
@@ -49,10 +185,14 @@ SELECT DISTINCT
       THEN 1
       ELSE 0
     END AS mortality_in 
+  , yug.hospitaldischargeoffset / 60 AS los_icu_hours
+
+  , COALESCE(transfusion_1.transfusion_yes, transfusion_2.transfusion_yes) AS transfusion_yes
+  , transfusion_units_day1
+  , transfusion_units_day2
 
 
-FROM `db_name.my_eICU.yugang` AS yug
-
+FROM `protean-chassis-368116.my_eICU.yugang` AS yug
 
 LEFT JOIN(
   SELECT patientunitstayid, unitvisitnumber, admissionweight, dischargeweight
@@ -63,7 +203,7 @@ ON icustay_detail.patientunitstayid = yug.patientunitstayid
 
 LEFT JOIN(
   SELECT patientunitstayid, MIN(adm_elective) AS adm_elective
-  FROM `db_name.my_eICU.pivoted_elective`
+  FROM `protean-chassis-368116.my_eICU.pivoted_elective`
   GROUP BY patientunitstayid
 )
 AS adm
@@ -71,9 +211,33 @@ ON adm.patientunitstayid = yug.patientunitstayid
 
 LEFT JOIN(
   SELECT *
-  FROM `db_name.my_eICU.pivoted_comorbidities`
+  FROM `protean-chassis-368116.my_eICU.pivoted_comorbidities`
 )
 AS coms
 ON coms.patientunitstayid = yug.patientunitstayid
+
+LEFT JOIN hb
+ON hb.patientunitstayid = yug.patienthealthsystemstayid
+
+LEFT JOIN lac_0
+ON lac_0.patientunitstayid = yug.patientunitstayid
+
+LEFT JOIN lac_1
+ON lac_1.patientunitstayid = yug.patientunitstayid
+
+LEFT JOIN lac_2
+ON lac_2.patientunitstayid = yug.patientunitstayid
+
+LEFT JOIN transfusion_1
+ON transfusion_1.patientunitstayid = yug.patientunitstayid
+
+LEFT JOIN transfusion_2
+ON transfusion_2.patientunitstayid = yug.patientunitstayid
+
+-- Inclusion Criteria for later
+-- WHERE lactate_day0 IS NOT NULL
+-- AND lactate_day1 IS NOT NULL
+-- AND lactate_day2 IS NOT NULL
+-- AND yug.hospitaldischargeoffset >= 1440
 
 ORDER BY yug.hospitalid, yug.wardid, yug.patienthealthsystemstayid, yug.patientunitstayid, yug.unitvisitnumber
