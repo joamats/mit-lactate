98f008ea94163720dd84cea216ba39b30493f2f0
João Matos <joaocarlosm00@gmail.com>
2023-02-13 12:35:48 -0500

    Add Fluids Query

--- src/sql/eICU/eICU_lactate.sql
+++ src/sql/eICU/eICU_lactate.sql
@@ -124,6 +124,104 @@ transfusion_2 AS (
   WHERE day = 2
 
   GROUP BY patientunitstayid
+),
+
+fluids AS (
+  SELECT
+      patientunitstayid
+    , intakeoutputoffset
+    , CASE
+        WHEN intakeoutputoffset >= 0     AND intakeoutputoffset < 1440   THEN 1
+        WHEN intakeoutputoffset >= 1440  AND intakeoutputoffset < 1440*2 THEN 2
+      ELSE NULL
+    END AS day
+    , cellvaluenumeric
+
+  FROM `physionet-data.eicu_crd.intakeoutput`
+
+  WHERE celllabel IN(
+    "5% Albumin"
+  , "25% Albumin"
+  , "albumin"
+  , "Colloids Total"
+  , "Crystalloid"
+  , "Crystalloids"
+  , "Albumin 5%"
+  , "25% Albumin 50 ml"
+  , "5% Albumin 500 ml"
+  , "Albumin 25%"
+  , "25% Albumin 100 ml"
+  , "Albumin Intake"
+  , "Saline Flush (mL)"
+  , "Lactated Ringers Volume"
+  , "Volume (mL)-lactated ringers infusion"
+  , "Volume (mL)-albumin human 5 % injection"
+  , "Volume (mL)-SODIUM CHLORIDE 0.9 % IV SOLN"
+  , "Volume (mL)-lactated ringers bolus 500 mL"
+  , "Volume (mL)-sodium chloride 0.9% infusion"
+  , "Volume (mL)-0.9 % sodium chloride solution"
+  , "Volume (mL)-0.45 % sodium chloride solution"
+  , "Volume (mL)-lactated ringers bolus 1,000 mL"
+  , "Volume (mL)-sodium chloride 0.45 % infusion"
+  , "Volume (mL)-albumin human 5 % injection 25 g"
+  , "Volume (mL)-0.45 % sodium chloride infusion"
+  , "Volume (mL)-0.9 %  sodium chloride infusion"
+  , "Volume (mL)-albumin human 5 % injection 12.5 g"
+  , "Volume (mL)-lactated ringers infusion 1,000 mL"
+  , "Volume (mL)-albumin human 25 % solution 25 g"
+  , "Volume (mL)-lactated ringers infusion 500 mL"
+  , "Volume (mL)-albumin human 25 % injection 25 g"
+  , "Volume (mL)-albumin human 5 % solution 12.5 g"
+  , "Volume (mL)-albumin human 5 % solution 250 mL"
+  , "Volume (mL)-sodium chloride 0.9 % bolus 250 mL"
+  , "Volume (mL)-sodium chloride 0.9 % bolus 500 mL"
+  , "Volume (mL)-sodium chloride 0.9 % 250 mL IV bolus"
+  , "Volume (mL)-sodium chloride 0.9 % 500 mL IV bolus"
+  , "Volume (mL)-albumin human 25 % injection 12.5 g"
+  , "Volume (mL)-sodium chloride 0.9 % bolus 1,000 mL"
+  , "Volume (mL)-albumin human (SPA) 25 % injection 25 g"
+  , "Volume (mL)-sodium chloride 0.9 % 1,000 mL IV bolus"
+  , "Volume (mL)-sodium chloride 0.9 % 1,000 mL infusion"
+  , "Volume (mL)-sodium chloride 0.9 % 2,000 mL IV bolus"
+  , "Volume (mL)-sodium chloride 0.9 % flush IVPB 250 mL"
+  , "Volume (mL)-sodium chloride 0.9 % flush IVPB 500 mL"
+  , "Volume (mL)-albumin human 5 % injection 12.5-25 g"
+  , "Volume (mL)-ALBUMIN HUMAN 5 % IV SOLN Pyxis Override"
+  , "Volume (mL)-albumin human (SPA) 25 % injection 12.5 g"
+  , "Volume (mL)-sodium chloride 0.45 % 1,000 mL infusion"
+  , "Volume (mL)-SODIUM CHLORIDE 0.9 % IV SOLN Pyxis Override"
+  , "Volume (mL)-sodium chloride (NORMAL SALINE) 0.9 % bolus 250 mL"
+  , "Volume (mL)-sodium chloride (NORMAL SALINE) 0.9 % bolus 500 mL"
+  , "Volume (mL)-sodium chloride (NORMAL SALINE) 0.9 % bolus 1,000 mL"
+  )
+),
+
+fluids_1 AS (
+
+  SELECT
+
+    patientunitstayid
+  , 1 AS fluids_yes
+  , SUM(cellvaluenumeric) AS fluids_sum_day1
+
+  FROM fluids
+  WHERE day = 1
+
+  GROUP BY patientunitstayid
+),
+
+fluids_2 AS (
+
+  SELECT
+
+    patientunitstayid
+  , 1 AS fluids_yes
+  , SUM(cellvaluenumeric) AS fluids_sum_day2
+
+  FROM fluids
+  WHERE day = 2
+
+  GROUP BY patientunitstayid
 )
 
 
@@ -187,9 +285,15 @@ SELECT DISTINCT
     END AS mortality_in 
   , yug.hospitaldischargeoffset / 60 AS los_icu_hours
 
+  -- Blood Transfusion
   , COALESCE(transfusion_1.transfusion_yes, transfusion_2.transfusion_yes) AS transfusion_yes
   , transfusion_units_day1
   , transfusion_units_day2
+
+  -- Fluids
+  , COALESCE(fluids_1.fluids_yes, fluids_2.fluids_yes) AS fluids_yes
+  , fluids_sum_day1
+  , fluids_sum_day2
 
 
 FROM `protean-chassis-368116.my_eICU.yugang` AS yug
@@ -233,6 +337,12 @@ ON transfusion_1.patientunitstayid = yug.patientunitstayid
 
 LEFT JOIN transfusion_2
 ON transfusion_2.patientunitstayid = yug.patientunitstayid
+
+LEFT JOIN fluids_1
+ON fluids_1.patientunitstayid = yug.patientunitstayid
+
+LEFT JOIN fluids_2
+ON fluids_2.patientunitstayid = yug.patientunitstayid
 
 -- Inclusion Criteria for later
 -- WHERE lactate_day0 IS NOT NULL
