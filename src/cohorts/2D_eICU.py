0677b7239081e499800c5ff1702a616e060ed4ff
Luca Weishaupt <53794882+luca-weishaupt@users.noreply.github.com>
2023-02-18 15:07:02 -0500

    get demographic data for flowchart

--- src/cohorts/2D_eICU.py
+++ src/cohorts/2D_eICU.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+from utils import get_demography, print_demo
 
 # get parent directory of this file
 script_dir = os.path.dirname(__file__)
@@ -11,29 +12,35 @@ root_dir = os.path.abspath(os.path.join(script_dir, os.pardir, os.pardir))
 df1 = pd.read_csv(os.path.join(root_dir, 'data', 'eICU_data.csv'))
 print(f"\n200859 stays in the ICU")
 print(f"Removed {200859 - len(df1)} stays without sepsis")
-print(f"{len(df1)} sepsis stays")
+demo1 = print_demo(get_demography(df1))
+print(f"{len(df1)} sepsis stays ({demo1})")
 
 df2 = df1[~df1.lactate_day1.isnull()]
 print(f"Removed {len(df1) - len(df2)} stays without lactate day 1")
-print(f"{len(df2)} stays with sepsis and lactate day 1")
+demo2 = print_demo(get_demography(df2))
+print(f"{len(df2)} stays with sepsis and lactate day 1 ({demo2})")
 
 df3 = df2[df2.los_icu_hours >= 24]
 print(f"Removed {len(df2) - len(df3)} stays with less than 24 hours")
-print(f"{len(df3)} stays with sepsis, lactate day 1 & 2, and LoS > 24h")
+demo3 = print_demo(get_demography(df3))
+print(f"{len(df3)} stays with sepsis, lactate day 1 & 2, and LoS > 24h ({demo3})")
 
 df4 = df3[df3.age >= 18]
 print(f"Removed {len(df3) - len(df4)} stays with non-adult patient")
-print(f"{len(df4)} stays with sepsis, lactate day 1 & 2, LoS > 24h, adult patient")
+demo4 = print_demo(get_demography(df4))
+print(f"{len(df4)} stays with sepsis, lactate day 1 & 2, LoS > 24h, adult patient ({demo4})")
 
 df5 = df4.sort_values(by=["patient_id", "stay_number"], ascending=True).groupby(
     'patient_id').apply(lambda group: group.iloc[0, 1:])
 print(f"Removed {len(df4) - len(df5)} recurrent stays")
-print(f"{len(df5)} adults with sepsis, lactate day 1 & 2, LoS > 24h, adult patient, 1 stay per patient")
+demo5 = print_demo(get_demography(df5))
+print(f"{len(df5)} adults with sepsis, lactate day 1 & 2, LoS > 24h, adult patient, 1 stay per patient ({demo5})")
 df5.to_csv(os.path.join(root_dir, 'data', 'cohort_eICU_lac1.csv'))
 
 df6 = df5[~df5.lactate_day2.isnull()]
 print(f"Removed {len(df5) - len(df6)} stays without lactate day 2")
-print(f"{len(df6)} stays with sepsis and lactate day 1 & 2")
+demo6 = print_demo(get_demography(df6))
+print(f"{len(df6)} stays with sepsis and lactate day 1 & 2 ({demo6})")
 df6.to_csv(os.path.join(root_dir, 'data', 'cohort_eICU_lac1_lac2.csv'))
 
 # df6.to_csv('data/cohort_eICU4.csv')
---
+++ src/cohorts/utils.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+
+def get_demography(df):
+    """Get the demography of the cohort.
+
+    Args:
+        df (pd.DataFrame): The cohort.
+    """
+    demo = {}
+    demo["race"] = {race: df[df["race_group"] == race].shape[0] /
+                         df.shape[0] for race in df["race_group"].unique() if race != "Other"}
+    demo["sex"] = {
+        "Male": df[df["sex_female"] == 0].shape[0] / df.shape[0],
+        "Female": df[df["sex_female"] == 1].shape[0] / df.shape[0]}
+    return demo
+
+
+def print_demo(demo):
+    demo_str = ""
+    for key, value in demo.items():
+        if isinstance(value, dict):
+            demo_str += f"{key}: ["
+            for key2, value2 in value.items():
+                demo_str += f"{key2}: {round(value2*100)}%, "
+            demo_str = demo_str[:-2] + "], "
+        else:
+            demo_str += f"{key}: {round(value*100)}%, "
+    demo_str = demo_str[:-2]
+    return demo_str
