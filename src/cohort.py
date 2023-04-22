f30df2776905f546cf1bd3272caae19ba3351327
Luca Weishaupt <53794882+luca-weishaupt@users.noreply.github.com>
2023-04-22 13:27:03 -0400

    make filepaths dynamic

--- src/cohort.py
+++ src/cohort.py
@@ -3,8 +3,12 @@ import numpy as np
 import os
 from utils import get_demography, print_demo
 
+# get project directory path (1 level up)
+PROJ_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(f"Project path: {PROJ_PATH}")
+
 # MIMIC
-df0 = pd.read_csv('data/MIMIC_data.csv')
+df0 = pd.read_csv(os.path.join(PROJ_PATH, 'data', 'MIMIC_data.csv'))
 print(f"\n{len(df0)} stays in the ICU")
 df1 = df0[df0.sepsis3 == 1]
 print(f"Removed {len(df0) - len(df1)} stays without sepsis")
@@ -28,4 +32,6 @@ print(f"Removed {len(df3) - len(df4)} stays with less than 1 day in ICU")
 demo4 = print_demo(get_demography(df4))
 print(f"{len(df3)} stays with sepsis, lactate day 1, and LoS > 1 day \n({demo4})\n")
 
-df4.to_csv('data/cohorts/MIMIC_lac1.csv')
+outdir = os.path.join(PROJ_PATH, 'data', 'cohorts')
+os.makedirs(outdir, exist_ok=True)
+df4.to_csv(os.path.join(outdir, 'MIMIC_lac1.csv'), index=False)
