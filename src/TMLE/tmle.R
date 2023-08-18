bbc76e6028aa67e8883decf38e4d67b6f9179fe5
Tristan <mirko84@yahoo.de>
2023-08-18 12:52:34 -0400

    tmle script

--- src/TMLE/tmle.R
+++ src/TMLE/tmle.R
@@ -1,9 +1,9 @@
-source("src/r_scripts/load_data.R")
-source("src/r_scripts/utils.R")
+library(tmle)
+library(tidyverse)
 
 # run TMLE 
 run_tmle <- function(data, treatment, confounders, outcome, SL_libraries,
-                     cohort, race, sev_min, sev_max, results_df) {
+                    results_df) {
 
     W <- data[, confounders]
     A <- data[, treatment]
@@ -22,15 +22,11 @@ run_tmle <- function(data, treatment, confounders, outcome, SL_libraries,
     log <- summary(result)   
 
     results_df[nrow(results_df) + 1,] <- c( outcome,
-                                            treatment,
-                                            cohort,
-                                            race,
-                                            sev_min,
-                                            sev_max,
-                                            log$estimates$ATE$psi,
-                                            log$estimates$ATE$CI[1],
-                                            log$estimates$ATE$CI[2],
-                                            log$estimates$ATE$pvalue,
+                                            treatment, # treatment = axis of race/sex/EP
+                                            log$estimates$OR$psi,
+                                            log$estimates$OR$CI[1],
+                                            log$estimates$OR$CI[2],
+                                            log$estimates$OR$pvalue,
                                             nrow(data),
                                             paste(SL_libraries$SL_library, collapse = " "),
                                             paste(result$Qinit$coef, collapse = " "),
@@ -40,92 +36,72 @@ run_tmle <- function(data, treatment, confounders, outcome, SL_libraries,
 }
 
 # Main
-cohorts <- c("MIMIC") # choose "MIMIC" only
-outcomes <- c("odd_hour") # "odd_hour","insulin_yes", "blood_yes", "comb_noso", "mortality_in"
-prob_mort_ranges <- read.csv("config/prob_mort_ranges.csv")
+outcomes <- c("lactate_day1_yes_no") # other outcomes
 treatments <- read.delim("config/treatments.txt")
-SL_libraries <- read.delim("config/SL_libraries_SL.txt") # or use only base libraries, see below
-#SL_libraries <- read.delim("config/SL_libraries_base.txt") # or read.delim("config/SL_libraries_SL.txt")
+# SL_libraries <- read.delim("config/SL_libraries_SL.txt") # or use only base libraries, see below
+SL_libraries <- read.delim("config/SL_libraries_base.txt") # or read.delim("config/SL_libraries_SL.txt")
+axis <- read.delim("config/axis.txt")
+print(axis)
+data <- read.csv("data/cohorts/cohort_MIMIC_lac1.csv")
+axis_values <- unique(data$gender)
+print(axis_values)
 
-
-for (c in cohorts) {
-    print(paste0("Cohort: ", c))
+for (a in axis) {
+   
+    print(paste0("Demographic: ", a))
 
     # Read Data for this database and cohort
-    data <- read.csv(paste0("data/", c, ".csv"))
+    data <- read.csv("data/cohorts/cohort_MIMIC_lac1.csv")
 
     # Factorize variables
+    confounders <- read.delim(paste0("config/confounders.txt"))
+ 
+    # Creating a list to store the unique values for each axis
+    print(a)
+    axis_values <- unique(data$a)
+    print(axis_values)
 
-    confounders <- read.delim(paste0("config/confounders_", c,".txt"))
+    # Setting the reference value for each axis and removing that axis from the confounders list
+    if (a == 'race_group') {
+        reference <- 'White'
+        axis_values <- axis_values[axis_values != reference]
+        conf_copy <- confounders[confounders != 'race_group']
 
-    for (outcome in outcomes) {
-        print(paste0("Outcome: ", outcome))
+    } else if (a == 'gender') {
+        reference <- 'M'
+        axis_values <- axis_values[axis_values != reference]
+        conf_copy <- confounders[confounders != 'sex_female']
 
-        # Dataframe to hold results
-        results_df <- data.frame(matrix(ncol=14, nrow=0))
-        colnames(results_df) <- c(
-                                "outcome",
-                                "treatment",
-                                "cohort",
-                                "race",
-                                "prob_mort_start",
-                                "prob_mort_end",
-                                "psi",
-                                "i_ci",
-                                "s_ci",
-                                "pvalue",
-                                "n",
-                                "SL_libraries",
-                                "Q_weights",
-                                "g_weights")
-
-        if (outcome == "mortality_in") {
-            races <- c("white", "non-white") 
-        } else {
-            races <- c("all") 
-        }
-
-        for (j in 1:nrow(treatments)) {
-            # Treatment
-            treatment <- treatments$treatment[j]
-            print(paste0("Treatment: ", treatment))
-
-            # Get formula with confounders and treatment
-            model_confounders <- read_confounders(j, treatments, confounders) 
-
-            for (r in races) {
-
-                print(paste0("Race: ", r))
-
-                if (r == "non-white") {
-                    subset_data <- subset(data, ethnicity_white == 0)
-
-                } else if (r == "white") {        
-                    subset_data <- subset(data, ethnicity_white == 1)
-                    
-                } else {
-                    subset_data <- data
-                }
-
-                for (i in 1:nrow(prob_mort_ranges)) {
-                    
-                    sev_min <- prob_mort_ranges$min[i]
-                    sev_max <- prob_mort_ranges$max[i]
-
-                    print(paste0("Stratification by prob_mort: ", sev_min, " - ", sev_max))
-
-                    # Stratify by prob_mort
-                    subsubset_data <- subset(subset_data, prob_mort >= sev_min & prob_mort < sev_max)
-                    
-                    # Run TMLE
-                    results_df <- run_tmle(subsubset_data, treatment, model_confounders, outcome, 
-                                           SL_libraries, c, r, sev_min, sev_max, results_df)
-
-                    # Save Results
-                    write.csv(results_df, paste0("results/prob_mort/", c, "/", outcome, ".csv"))
-
-                }
-            }           
-        }
+    } else {
+        reference <- 'ENGLISH'
+        axis_values <- axis_values[axis_values != reference]
+        conf_copy <- confounders[confounders != 'eng_prof']
     }
+
+    # Encoding columns
+    for (col in axis_values) {
+        data[[col]] <- ifelse(data[[a]] == col, 1,
+                              ifelse(data[[a]] == reference, 0, NA))
+    }
+
+    # Dataframe to hold results
+    results_df <- data.frame(matrix(ncol=10, nrow=0))
+    colnames(results_df) <- c(
+                            "outcome",
+                            "tx_demographic",
+                            "OR",
+                            "i_ci",
+                            "s_ci",
+                            "pvalue",
+                            "n",
+                            "SL_libraries",
+                            "Q_weights",
+                            "g_weights")
+
+    # Run TMLE
+    results_df <- run_tmle(data, treatment, model_confounders, outcome, 
+                            SL_libraries, results_df)
+
+    # Save Results
+    write.csv(results_df, paste0("results/tmle/tmle_", a, ".csv"))
 }
